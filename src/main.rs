f2a04d571563134d4545d80f70ff84c68b3c734d
JosephAbbey <66320873+LabyrinthLion@users.noreply.github.com>
2022-03-28 20:11:23 +0100

    calculus testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod classes;
 mod syntax;
 
-use classes::{Polynomial, Term};
+use classes::{Calculus, Polynomial};
 
 fn parse(input: String) -> Box<Polynomial> {
   let mut parser = syntax::Parser::new(input);
@@ -9,13 +9,8 @@ fn parse(input: String) -> Box<Polynomial> {
 }
 
 fn main() {
-  let t = Polynomial::new(vec![
-    Term::new(0f64, 0f64),
-    Term::new(1f64, 0f64),
-    Term::new(1f64, 1f64),
-    Term::new(2f64, 2f64),
-  ]);
-  println!("{:#}", t);
   let t = parse("0 + 1 + x + 2x^2".to_string());
   println!("{:#}", t);
+  println!("{:#}", t.derivative());
+  println!("{:#}", t.integral());
 }
