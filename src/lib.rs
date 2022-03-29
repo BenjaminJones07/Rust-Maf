22cb99b9f53498c5d1084422da5a94c6ed8f1ffa
Joseph Abbey <joseph@abbey1.org.uk>
2022-03-29 13:14:22 +0000

    printing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
+mod classes;
 
+#[cfg(test)]
+mod tests {
+  use crate::classes::{Term, Polynomial};
+
+  #[test]
+  fn test() {
+    let t = Term::new(1f64, 1f64);
+    println!("{:#}", t);
+    assert_eq!(1, 1)
+  }
+
+  #[test]
+  fn printing() {
+    let t = Polynomial::new(vec![
+      Term::new(0f64, 0f64),
+      Term::new(1f64, 0f64),
+      Term::new(1f64, 1f64),
+      Term::new(2f64, 2f64),
+    ]);
+    assert_eq!(format!("{:#}", t), "0 + 1 + x + 2x^2")
+  }
+}
