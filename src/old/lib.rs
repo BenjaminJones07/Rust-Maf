3ee85d53e1d587b1caf7ec2e457604b3285fe428
JosephAbbey <66320873+LabyrinthLion@users.noreply.github.com>
2022-04-01 16:33:51 +0100

    deprecate old version

---
+++ src/old/lib.rs
@@ -0,0 +1,32 @@
+mod classes;
+
+#[cfg(test)]
+mod tests {
+  use crate::classes::{Calculus, Polynomial, Term, Trig};
+
+  #[test]
+  fn test1() {
+    let t1 = Term::new(3f64, vec![], 4f64); // 3x^4 (Derivative: 12x^3)
+    let t2 = Term::new(12f64, vec![], 3f64); // 12x^3
+
+    assert_eq!(format!("{:#}", t1.derivative()), format!("{:#}", *t2))
+  }
+
+  #[test]
+  fn printing() {
+    let t = Polynomial::new(vec![
+      Term::new(0f64, vec![], 0f64), //0
+      Term::new(1f64, vec![], 0f64), //1
+      Term::new(1f64, vec![], 1f64), //x
+      Term::new(2f64, vec![], 2f64), //2x^2
+    ]);
+    assert_eq!(format!("{:#}", t), "0 + 1 + x + 2x^2")
+  }
+
+  #[test]
+  fn trig() {
+    let t = Term::new(1f64, vec![], 3f64);
+    let s = Trig::Sin(t);
+    println!("{:#}", s);
+  }
+}
